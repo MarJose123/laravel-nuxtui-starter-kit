e6ad5e79ce0ea7a0747a1d57b1b86c665358359f
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-09-20 23:30:26 +0800

    WIP

---
+++ vite.config.js
@@ -0,0 +1,46 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
+import vue from '@vitejs/plugin-vue';
+import ui from '@nuxt/ui/vite'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/js/app.ts'],
+            ssr: 'resources/js/ssr.ts',
+            refresh: true,
+        }),
+        tailwindcss(),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+        ui({
+            inertia: true,
+            components: {
+                dirs: ['resources/js/components'],
+            },
+            ui: {
+                colors: {
+                    primary: 'scarlet',
+                    neutral: 'neutral',
+                },
+            },
+        }),
+        vueDevTools({
+            appendTo: 'resources/js/app.ts'
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './resources/js'),
+        },
+    },
+});
