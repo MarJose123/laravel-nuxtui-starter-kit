fa803bd786d293fe2dbe6a020cab1d426096c046
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-09-25 19:39:58 +0800

    Refactor `RegisterController` to `RegisterUserController` and streamline registration workflow. Add TypeScript types for notifications and model resources.

--- app/Http/Controllers/Auth/RegisterUserController.php
+++ app/Http/Controllers/Auth/RegisterUserController.php
@@ -5,15 +5,16 @@ namespace App\Http\Controllers\Auth;
 use App\Actions\User\CreateUser;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\RegisterRequest;
-use App\Services\InertiaNotification;
-use Illuminate\Http\Request;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
+use Inertia\Response;
 
-class RegisterController extends Controller
+class RegisterUserController extends Controller
 {
     public function __construct(private readonly CreateUser $createUser) {}
 
-    public function create(Request $request)
+    public function create(): Response
     {
         return Inertia::render('auth/Register');
     }
@@ -23,15 +24,13 @@ class RegisterController extends Controller
      */
     public function store(RegisterRequest $request)
     {
-        $this->createUser->handle($request->validated());
+        $user = $this->createUser->handle($request->validated());
 
-        InertiaNotification::make()
-            ->success()
-            ->message('User created successfully.')
-            ->title('Success')
-            ->send();
+        event(new Registered($user));
 
-        return to_route('login');
+        Auth::login($user);
+
+        return to_route('dashboard');
 
     }
 }
