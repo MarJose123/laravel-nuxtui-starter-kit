4fb565b1d01126f16ac5a8235b56e349ef930822
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-09-25 20:02:21 +0800

    Add password reset functionality with `NewPasswordController` and `ResetPassword` page

---
+++ app/Http/Requests/Auth/NewPasswordRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
+
+class NewPasswordRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'token'    => ['required'],
+            'email'    => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'confirmed',
+                Password::min(8)
+                    ->letters()
+                    ->numbers()
+                    ->mixedCase() // lower-case and upper-case letter
+                    ->symbols()
+                    ->uncompromised(),
+            ],
+            'password_confirmation' => ['required'],
+        ];
+    }
+
+    public function authorize(): bool
+    {
+        return true;
+    }
+}
---
+++ resources/js/pages/auth/ResetPassword.vue
@@ -0,0 +1,112 @@
+<script setup lang="ts">
+import Logo from '@/components/Logo.vue'
+import UPasswordInput from '@/components/ui/UPasswordInput.vue'
+import Layout from '@/layouts/auth.vue'
+import { Head, useForm } from '@inertiajs/vue3'
+import { ref } from 'vue'
+
+defineOptions({ layout: Layout })
+
+const props = defineProps<{
+    token: string
+    email: string
+}>()
+
+const show = ref(false)
+
+const form = useForm({
+    token: props.token,
+    email: props.email,
+    password: '',
+    password_confirmation: '',
+})
+
+const onSubmit = () => {
+    console.log('Submitted', form)
+
+    form.clearErrors()
+
+    form.post(route('register.store'), {
+        onSuccess: () => {
+            form.reset()
+        },
+        onError: () => {
+            form.reset('password_confirmation')
+        },
+    })
+}
+</script>
+
+<template>
+    <div>
+        <Head title="Register" />
+
+        <!-- START: Register Form Root Layout -->
+        <div class="w-full max-w-sm space-y-6">
+            <!--  START:  Header Section        -->
+            <div class="flex flex-col text-center">
+                <div class="mb-2">
+                    <Logo class="inline-block size-8 shrink-0" />
+                </div>
+
+                <div class="text-xl font-semibold text-pretty text-highlighted">Create an account</div>
+                <div class="mt-1 text-base text-pretty text-muted">Enter your details below to create your account</div>
+            </div>
+            <!--   END: Header Section        -->
+
+            <!--  START:  Form Section       -->
+            <div class="flex flex-col gap-y-6">
+                <UForm class="space-y-5" @submit.prevent="onSubmit">
+                    <UFormField label="Email" name="email" :error="form.errors.email" required>
+                        <UInput class="w-full" v-model="form.email" placeholder="Enter your email" />
+                    </UFormField>
+
+                    <UPasswordInput v-model="form.password" label="Password" :error="form.errors.password" required />
+
+                    <UFormField
+                        label="Confirm Password"
+                        name="password_confirmation"
+                        :error="form.errors.password_confirmation"
+                        required
+                    >
+                        <UInput
+                            v-model="form.password_confirmation"
+                            placeholder="Confirm your password"
+                            :type="show ? 'text' : 'password'"
+                            :ui="{ trailing: 'pe-1' }"
+                            class="w-full"
+                        >
+                            <template #trailing>
+                                <UButton
+                                    color="neutral"
+                                    variant="link"
+                                    size="sm"
+                                    :icon="show ? 'i-lucide-eye-off' : 'i-lucide-eye'"
+                                    :aria-label="show ? 'Hide password' : 'Show password'"
+                                    :aria-pressed="show"
+                                    aria-controls="password"
+                                    @click="show = !show"
+                                />
+                            </template>
+                        </UInput>
+                    </UFormField>
+
+                    <UButton
+                        label="Reset Password"
+                        type="submit"
+                        block
+                        :disabled="form.processing"
+                        :loading="form.processing"
+                        class="w-full"
+                    />
+                </UForm>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+::-ms-reveal {
+    display: none;
+}
+</style>
