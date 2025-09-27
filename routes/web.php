183da9d31e37c6be80490ff34179d6dd6364d9c0
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-09-27 13:16:25 +0800

    Include account settings routes in web.php.

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,4 @@ Route::get('/', fn () => Inertia::render('Welcome'))->name('home');
 Route::get('/dashboard', fn () => Inertia::render('Dashboard'))->name('dashboard')->middleware(['auth', 'verified']);
 
 require __DIR__.'/auth/auth.php';
+require __DIR__.'/settings/account.php';
