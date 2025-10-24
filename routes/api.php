c4b10df02e331e59bd558d4c1a75eef4781c287d
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-10-24 20:53:13 +0800

    refactor: Remove direct user route from api.php

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,4 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/user', fn (Request $request) => $request->user('users-api'))->middleware('auth:users-api');
-
 require __DIR__.'/api/auth/auth.php';
 require __DIR__.'/api/profile/user.php';
