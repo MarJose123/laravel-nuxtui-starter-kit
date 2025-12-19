998787bd414ba30bc0ef042a5f3f99f0b3624994
Josie Darang <18107626+MarJose123@users.noreply.github.com>
2025-12-19 12:01:42 +0800

    refactor: User Menu  (#17)
    * refactor: Extract UserMenu component
    
    * test: Improve logout test selector

--- tests/Browser/Auth/AuthenticationTest.php
+++ tests/Browser/Auth/AuthenticationTest.php
@@ -52,7 +52,8 @@ test('users can logout', function (): void {
     actingAs($user);
 
     visit(route('dashboard'))
-        ->click('#dashboard-sidebar-v-0 > div.shrink-0.flex.items-center.gap-1\.5.px-4.py-2.border-t.border-default > div > button')
+        ->click('#reka-dropdown-menu-trigger-v-3')
+        ->click('.logout')
         ->assertUrlIs(route('home'))
         ->assertNoConsoleLogs()
         ->assertNoJavaScriptErrors();
